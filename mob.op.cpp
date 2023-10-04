a6da33e09648e7424701a6101c91362b19594db4
BeslanPorsche911 <146226227+BeslanPorsche911@users.noreply.github.com>
2023-10-04 22:49:21 +0300

    Add files via upload

---
+++ mob.op.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	float a;
+	short b;
+	cout << "время вашего разговора(минут) ? ";
+	cin >> a;
+	if (a >= 0) 
+		cout << "Ваш мобильный оператор: 1,2,3\n";
+		cin >> b;
+		switch (b) {
+		case 1:
+			cout << "сумма вашего разговора вышел " <<  a * 2 << " долларов";
+			break;
+		case 2:
+			cout << "сумма вашего разговора вышел " <<  a *  2.7 << " долларов";
+			break;
+		case 3:
+			cout << "сумма вашего разговора вышел " <<  a * 3 << " долларов";
+			break;
+		default:
+			cout << "Введенные данные некорректны";
+		}
+
+	
+
+	
+
+	
+
+	
+	
+	
+}
+

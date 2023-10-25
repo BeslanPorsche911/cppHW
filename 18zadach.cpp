a242f0818a7b271438fa1dd62e67bfe636568474
BeslanPorsche911 <146226227+BeslanPorsche911@users.noreply.github.com>
2023-10-25 16:02:07 +0300

    Add files via upload

---
+++ 18zadach.cpp
@@ -0,0 +1,208 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        // ZADANIE 1
+        // №1 пользователь вводит букву и надо его вывести
+        // char a;
+        // cin>>a;
+        // cout<<a
+        // №2 пользователь вводит число и надо его вывести
+        // int a;
+        // cin>>a;
+        // cout<<a;
+        // №3 пользователь выводит свое имя
+        // char a;
+        // cin >>a;
+        // cout << a;
+        //ZADANIE 2
+        // №4 пользователь вводит число и надо вывести куб этого числа
+        // int a;
+        // cin>>a;
+        // cout<< a*a*a<<" kub chisla";
+        // №5 пользователь вводит числа и найти их сумму и разницу
+        // int a,b,c;
+        // cin >>a;
+        // cin>>b;
+        // cout <<a+b"summa";
+        // cout<< a/b"razn";
+        // №6  пользователь вводит числа ,нужно найти средне арифметическое 3 чисел
+        // int a,b,c;
+        // cin >>a;
+        // cin >>b;
+        // cin>>c;
+        // cout << (a+b+c)/3;
+        // ZADANIE 3
+        // №7 пользователь вводит 2 числа и нужно узнать какая из них больше
+        // int a,b;
+        // cin >>a;
+        // cin >>b;
+        // if (a>b){
+        //     cout << a << " bolshe";
+        // }
+        // else {
+        //     cout << b << " bolshe";
+        // }
+        // №8 пользователь вводит число и если оно больше 25 то он побеждает
+        // int a;
+        // cin >>a;
+        // if (a>25){
+        //     cout<<a<<"polzovatel pobedil";
+        // }
+        // else {
+        //     cout <<"polzovatel proigral";
+        // }
+        // №9 пользователь вводит число и должны узнать, кратно оно 2 или нет
+        // int a;
+        // cin>>a;
+        // if (a%2==0){
+        //     cout <<a<<" kratno 2";
+        // }
+        // else {
+        //     cout<<" ne kratno 2";
+        // }
+        // ZADANIE 4
+        // №10 пользователь вводит число и надо узнать пол,отриц или равна нулю
+        // int a;
+        // cin >>a;
+        // cout <<a;
+        // if(a>0){
+        //     cout<<" pol";
+        // }
+        // else if (a <0){
+        //     cout<< " otric";
+        // }
+        // else {
+        //     cout << " chislo ravno 0";
+        // }
+        // №11 пользователь вводит сколько ему лет и должен узнать в какую возрастную категорию он входит
+        // int a;
+        // cin>> a;
+        // cout <<a;
+        // if(a>18){
+        // cout << " soverhenn";
+        // }
+        // else if(a>=14 && a<=18){
+        //     cout <<" podrostok";
+        // }
+        // else {
+        //     cout << " molokosos";
+        // }
+        // №12 пользователь вводит цену на такси ,а ему выводятся тариф на данную сумму
+        // int a;
+        // cin >>a;
+        // cout <<a;
+        // if (a>=499 && a<=200){
+        //     cout <<"detskiy";
+        // }
+        // else if (a>500 && a<=700){
+        // cout <<" komfort";
+        // }
+        // else if(a>700 && a<=1250){
+        // cout <<" biznes";
+        // }
+        // else {
+        // cout <<" net takih";
+        // }
+        // ZADANIE 5
+        // №13 найти какое число больше
+        // int a,b;
+        // cin >>a;
+        // cin>>b;
+        // cout <<((a>b || a<b)? "a":"b");
+        // №14 узнать входил ли число в диапозон
+        // int a,b,c;
+        // cout <<"vvedite 1 diap ";
+        // cin>>a;
+        // cout <<"vvedite 2 diap ";
+        //  cin>>b;
+        // cout<<"vvedite chislo ";
+        // cin>>c;
+        // cout<<((c>=a&&c<=b)? "vhodit v diap":"ne vhodit");
+        // №15 узнать четное число или нет
+        // int a;
+        // cout<<"vvedite chislo ";
+        // cin>>a;
+        // cout<<((a%2==0)? "chetnoe":"nechetnoe");
+        // ZADANIE 6
+        // №16 пользователь пишет цифры ,вывести их на английском языке
+        // int a;
+        // cout <<"vvedite chislo ";
+        // cin >>a;
+        // switch (a) {
+        //     case 0:
+        //     cout << "zero";
+        //     break;
+        //     case 1:
+        //     cout << "one";
+        //     break;
+        //     case 2:
+        //     cout << "two";
+        //     break;
+        //     case 3:
+        //     cout << "three";
+        //     break;
+        //     case 4:
+        //     cout << "four";
+        //     break;
+        //     case 5:
+        //     cout << "five";
+        //     break;
+        //     case 6:
+        //     cout << "six";
+        //     break;
+        //     case 7:
+        //     cout << "seven";
+        //     break;
+        //     case 8:
+        //     cout << "eight";
+        //     break;
+        //     case 9:
+        //     cout << "nine";
+        //     break;
+        //     default:
+        //     cout <<"error";
+        // }
+    /*№17 надо ввести оценку студента.
+    int a;
+    cout << "vveditre ocenku studenta: ";
+    cin >> a;
+
+    switch (a) {
+    case 5:
+        cout << "nice";
+        break;
+    case 4:
+        cout << "good";
+        break;
+    case 3:
+        cout << "satisfactory";
+        break;
+    case 2:
+        cout << "bad";
+        break;
+    default:
+        cout << "Error";
+    }*/
+    	/*int a;
+    	cout << "vvedite znachenie: ";
+   	cin >> a;
+   switch (a) {
+    	case 0:
+    	case 1:
+    	case 2:
+    	case 3:
+    	case 5:
+    	case 6:
+    	case 7:
+    	case 8:
+    	case 9:
+    		cout << "cifra ";
+    		break;
+    	default:
+    		cout << "chislo ";*/
+   }
+}
+

76d56768f79ec20583901f1ac81423879e140ed5
BeslanPorsche911 <146226227+BeslanPorsche911@users.noreply.github.com>
2023-12-13 17:07:15 +0300

    Add files via upload

---
+++ zadachi na massiv.cpp
@@ -0,0 +1,54 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//массив находит нечетное элемент
+   // int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+   // int length = sizeof(arr) / sizeof(arr[0]);
+
+    //for (int i = 0; i < length; i++) {
+    //    if (arr[i] % 2 != 0) {
+   //         cout << arr[i] << " ";
+   //     }
+   // }
+
+   // return 0;
+
+    //удаляет элемент из позиции
+    const int MAX_SIZE = 10;
+    int arr[MAX_SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int size = 10;
+    int position = 5;
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    if (position >= 0 && position < size) {
+        for (int i = position; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        size--;
+
+        cout << "Измененный массив: ";
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    else {
+        cout << "Ошибка: недопустимая позиция!" << endl;
+    }
+
+    return 0;
+
+
+}
+
+
+
